6c0f4890ec2cd898e9b6f9623c0043898e4c9eec
waithope <yunqiang.gan@gmail.com>
2018-05-18 15:48:12 +0800

    update dotfile

--- ycm_config/.ycm_extra_conf.py
+++ ycm_config/.ycm_extra_conf.py
@@ -1,36 +1,3 @@
-# YCM_CONFIG_BACKUP
-
-
-# This file is NOT licensed under the GPLv3, which is the license for the rest
-# of YouCompleteMe.
-#
-# Here's the license text for this file:
-#
-# This is free and unencumbered software released into the public domain.
-#
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
-#
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-#
-# For more information, please refer to <http://unlicense.org/>
-
 from distutils.sysconfig import get_python_inc
 import platform
 import os
@@ -42,55 +9,57 @@ import ycm_core
 flags = [
 '-Wall',
 '-Wextra',
-'-Werror',
+#'-Werror',
 '-Wno-long-long',
 '-Wno-variadic-macros',
+# '-Wno-unused-parameter',
 '-fexceptions',
 '-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
+
+# For a C project, you would set this to something like 'c99' instead of
+# 'c++11'.
+
+'-std=c99',
 # THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
 # use when compiling headers. So it will guess. Badly. So C++ headers will be
 # compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
 # For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c',
-'-isystem',
-'../BoostParts',
-'-isystem',
-get_python_inc(),
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
-'-isystem',
-'./benchmarks/benchmark/include',
-'-isystem',
-'/usr/include',
-'-isystem',
-'/usr/local/include',
-'-isystem',
-'/usr/include/c++/5.4.0', 
+'-x', 'c',
+
+'-isystem', '../BoostParts',
+
+'-isystem', 'get_python_inc()',
+
+'-isystem', '../llvm/include',
+
+'-isystem', '../llvm/tools/clang/include',
+
+'-I', '.',
+
+'-I', './ClangCompleter',
+
+'-isystem', './tests/gmock/gtest',
+
+'-isystem', './tests/gmock/gtest/include',
+
+'-isystem', './tests/gmock',
+
+'-isystem', './tests/gmock/include',
+
+'-isystem', './benchmarks/benchmark/include',
+
+'-isystem', '/usr/include',
+
+'-isystem', '/usr/local/include',
+
+'-isystem', '/usr/include/c++/5.4.0', 
 ]
 
 # Clang automatically sets the '-std=' flag to 'c++14' for MSVC 2015 or later,
 # which is required for compiling the standard library, and to 'c++11' for older
 # versions.
 if platform.system() != 'Windows':
-  flags.append( '-std=c++11' )
+  flags.append( '-std=c99' )
 
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
